5355d332362cfeed3ccb1a05f3d81e4f183ea81b
feiquan <feiquanxx@gmail.com>
2022-03-05 11:53:41 +0800

    修改前端页面 (#1)
    * picture style
    
    * 移除无效代码
    
    Co-authored-by: hu chunxaing <1623481484@qq.com>

--- ui/src/views/model_introduction/admin/index.vue
+++ ui/src/views/model_introduction/admin/index.vue
@@ -23,26 +23,22 @@
 <script>
 import GithubCorner from '@/components/GithubCorner'
 import IntroductionPanel from './components/IntroductionPanel'
-
 // 文案内容
 import Introduction from './components/introduction/Introduction'
 import TheoriesTechnology from './components/introduction/TheoriesTechnology'
 import ImagePreprocessing from './components/introduction/ImagePreprocessing'
 import BtSeg from './components/introduction/BtSeg'
-
 const titles = {
   introduction: Introduction,
   theories_technology: TheoriesTechnology,
   image_preprocessing: ImagePreprocessing,
   bt_seg: BtSeg
 }
-
 export default {
   name: 'ModelIntroductionAdmin',
   components: {
     GithubCorner,
     IntroductionPanel,
-
     Introduction,
     TheoriesTechnology,
     ImagePreprocessing,
@@ -71,39 +67,33 @@ export default {
   font-weight:bold;
   color:#103838;
 }
-
 .msg{
   line-height:2.5em;
   word-break:break-all;
   padding: 16px;
   color:#185757;
 }
-
 .msg-pre{
   text-indent: 2em;
   white-space:pre-wrap;
   word-wrap:normal;
 }
-
 .introduction-editor-container {
   padding: 32px;
   background-color: rgb(240, 242, 245);
   position: relative;
-
   .github-corner {
     position: absolute;
     top: 0px;
     border: 0;
     right: 0;
   }
-
   .chart-wrapper {
     background: #fff;
     padding: 0px 16px 16px 0px;
     margin-bottom: 32px;
   }
 }
-
 @media (max-width:1024px) {
   .chart-wrapper {
     padding: 8px;
